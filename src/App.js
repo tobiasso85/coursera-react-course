5b076176e8dc731a56c438deb192f8ee531d0cd4
Tobias Sorn <tobias.sorn@sap.com>
2025-11-24 17:47:44 +0100

    hamburger

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ConfirmedBooking from './comps/bookings/confirmedBooking';
 import Footer from './comps/footer';
 import Navbar from './comps/navigationbar';
+import About from './pages/about';
+import Home from './pages/home';
+import Login from './pages/login';
+import Menu from './pages/menu';
+import OrderOnline from './pages/orderOnline';
+import Reservations from './pages/reservations';
 
 
-
+const internalLinks = [{
+  href: "/",
+  name: "Home",
+  comp: Home
+}, {
+  href: "/about",
+  name: "About",
+  comp: About
+}, {
+  href: "/menu",
+  name: "Menu",
+  comp: Menu
+}, {
+  href: "/reservations",
+  name: "Reservations",
+  comp: Reservations
+}, {
+  href: "/orderonline",
+  name: "Order Online",
+  comp: OrderOnline
+}, {
+  href: "/login",
+  name: "Login",
+  comp: Login
+}, {
+  href: "/confirmed",
+  name: "",
+  comp: ConfirmedBooking
+}]
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar></Navbar>
+        <Navbar internalLinks={internalLinks}></Navbar>
+        <main className="themain">
+
+          <Routes>
+            {internalLinks.map((e, i) => {
+              const Comp = e.comp;
+              return <Route key={i} path={e.href} element={<Comp></Comp>}></Route>
+            })}
+          </Routes>
+        </main>
         <Footer></Footer>
       </BrowserRouter>
     </div>
--- src/comps/navigationbar.jsx
+++ src/comps/navigationbar.jsx
@@ -1,83 +1,63 @@
-import About from "../pages/about";
-import Home from "../pages/home";
-import Login from "../pages/login";
-import Menu from "../pages/menu";
-import OrderOnline from "../pages/orderOnline";
-import Reservations from "../pages/reservations";
-import ConfirmedBooking from "./bookings/confirmedBooking";
+import { useState } from "react";
 import logoSvg from "./logo.svg";
 import "./navigationbar.css"; // optional if you use the CSS below
 
 
-import { Link, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const internalLinks = [{
-  href: "/",
-  name: "Home",
-  comp: Home
-}, {
-  href: "/about",
-  name: "About",
-  comp: About
-}, {
-  href: "/menu",
-  name: "Menu",
-  comp: Menu
-}, {
-  href: "/reservations",
-  name: "Reservations",
-  comp: Reservations
-}, {
-  href: "/orderonline",
-  name: "Order Online",
-  comp: OrderOnline
-}, {
-  href: "/login",
-  name: "Login",
-  comp: Login
-}, {
-  href: "/confirmed",
-  name: "",
-  comp: ConfirmedBooking
-}]
 
-const Navbar = () => {
+
+const Navbar = ({ internalLinks }) => {
+
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <>
-    <header>
+      <header>
 
-      <nav className="navbar">
-        <div className="nav-left">
-          <img src={logoSvg} alt="logo"></img>
-        </div>
+        <nav className="navbar">
+          <div className="nav-left">
+            <img src={logoSvg} alt="logo"></img>
+          </div>
 
-        <div className="nav-right">
-          <ul>
-            {
-              internalLinks.filter(x => x.name).map((e, i) => {
-                return (
-                  <li key={i} >
-                    <Link to={e.href} className="navItem">{e.name}</Link>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      </nav>
+          <button 
+          className="hamburger" 
+          type="button" 
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ?
+            <div>
+              <div className="bar1x"></div>
+              <div className="bar2x"></div>
+              <div className="bar3x"></div>
+            </div> : 
+            <div>
+              <div className="bar1"></div>
+              <div className="bar2"></div>
+              <div className="bar3"></div>
+            </div>
+}
+        </button>
 
-    </header>
-      <main className="themain">
+          <div className={expanded ? 'navbarlinks expanded' : 'navbarlinks'} 
+          onClick={() => setExpanded(!expanded)}>
+            <ul>
+              {
+                internalLinks.filter(x => x.name).map((e, i) => {
+                  return (
+                    <li key={i} >
+                      <Link to={e.href} className="navItem">{e.name}</Link>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </nav>
 
-      <Routes>
-        {internalLinks.map((e, i) => {
-          const Comp = e.comp;
-          return <Route key={i} path={e.href} element={<Comp></Comp>}></Route>
-        })}
-      </Routes>
-        </main>
-        </>
+      </header>
+
+    </>
   );
 };
 
